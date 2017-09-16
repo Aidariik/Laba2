a041b411cfde7e66838eb5dce4d0bd622e246eb6
Aidariik <mazhenovaidar@gmail.com>
2017-09-16 12:23:42 +0600

    Pr

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double c = Math.Sqrt(a*a + b*b);
+            System.Console.WriteLine(c);
+        }
+    }
+}
