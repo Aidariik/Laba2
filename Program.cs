dcc25ccd334b0746ffdbb3eb97b0408bf3089045
Aidariik <mazhenovaidar@gmail.com>
2017-09-16 12:44:58 +0600

    ewe raz1

--- Program.cs
+++ Program.cs
@@ -15,7 +15,13 @@ namespace OOP
             System.Console.Writeline("Let the bodies hit the floor");
             System.Console.Writeline("Let the bodies hit the ...");
             System.Console.Writeline("Floooooooooooooooor!");
-	    System.Console.Writeline("Chorus:Here we go,here we go")            
+	    System.Console.Writeline("Chorus:Here we go,here we go");
+        System.Console.Writeline("Chorus:Here we go,here we go123123") ;
+        System.Console.Writeline("Chorus:Here we go,123123here we go") 
+        System.Console.Writeline("Chorus:Here we go,he123123123re we go") 
+        System.Console.Writeline("Chorus:Her23123123e we go,here we go") 
+        System.Console.Writeline("Chorus:Her1231e we123123 go,here we go") 
+        System.Console.Writeline("Chorus:Here we123123 go,here we go")            
         }
     }
 }
